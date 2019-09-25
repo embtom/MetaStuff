6a65027b1ae5905de5e245613554601f7bd0c27a
Daniel Zicsi-Liess <liessdaniel415@gmail.com>
2019-09-25 19:08:39 +0200

    added json serialization/deserialization support for std::optional and std::variant

--- example/JsonCast.h
+++ example/JsonCast.h
@@ -3,19 +3,19 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
+#include <optional>
+#include <variant>
 
 #include <json.hpp>
 
 #include <Meta.h>
 #include "StringCast.h"
 
-using json = nlohmann::json;
+template <typename T>
+void to_json(nlohmann::json& j, const T& obj);
 
 template <typename T>
-void to_json(json& j, const T& obj);
-
-template <typename T>
-void from_json(const json& j, T& obj);
+void from_json(const nlohmann::json& j, T& obj);
 
 namespace meta
 {
@@ -24,47 +24,59 @@ namespace meta
 
 template <typename Class,
     typename = std::enable_if_t <meta::isRegistered<Class>()>>
-json serialize(const Class& obj);
+nlohmann::json serialize(const Class& obj);
 
 template <typename Class,
     typename = std::enable_if_t <!meta::isRegistered<Class>()>,
     typename = void>
-json serialize(const Class& obj);
+nlohmann::json serialize(const Class& obj);
 
 template <typename Class>
-json serialize_basic(const Class& obj);
+nlohmann::json serialize_basic(const Class& obj);
 
 // specialization for std::vector
 template <typename T>
-json serialize_basic(const std::vector<T>& obj);
+nlohmann::json serialize_basic(const std::vector<T>& obj);
 
 // specialization for std::unodered_map
 template <typename K, typename V>
-json serialize_basic(const std::unordered_map<K, V>& obj);
+nlohmann::json serialize_basic(const std::unordered_map<K, V>& obj);
 
+// specialization for std::variant
+template <typename... T>
+nlohmann::json serialize_basic(const std::variant<T...>& obj);
+inline
+nlohmann::json serialize_basic(const std::monostate& obj);
 
 /////////////////// DESERIALIZATION
 //
-//template<typename Class>
-//Class deserialize(const json& obj);
 
 template <typename Class,
     typename = std::enable_if_t<meta::isRegistered<Class>()>>
-void deserialize(Class& obj, const json& object);
+void deserialize(Class& obj, const nlohmann::json& object);
 
 template <typename Class,
     typename = std::enable_if_t<!meta::isRegistered<Class>()>,
     typename = void>
-void deserialize(Class& obj, const json& object);
+void deserialize(Class& obj, const nlohmann::json& object);
+
+template <typename Class>
+void deserialize_basic(Class& obj, const nlohmann::json& object);
 
 // specialization for std::vector
 template <typename T>
-void deserialize(std::vector<T>& obj, const json& object);
+void deserialize_basic(std::vector<T>& obj, const nlohmann::json& object);
 
 // specialization for std::unodered_map
 template <typename K, typename V>
-void deserialize(std::unordered_map<K, V>& obj, const json& object);
+void deserialize_basic(std::unordered_map<K, V>& obj, const nlohmann::json& object);
 
-}
+// specialization for std::variant
+template <typename... T>
+void deserialize_basic(std::variant<T...>& ret, const nlohmann::json& object);
+inline
+void deserialize_basic(std::monostate& ret, const nlohmann::json& object);
+
+} // namespace meta
 
 #include "JsonCast.inl"
